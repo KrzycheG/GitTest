c0fc98dd5618ebfeda12a9338aff9ad9a72e7137
pusio <kpstrowski@gmail.com>
2021-10-23 14:38:43 +0200

    Stworzenie Squash

--- src/main/java/Testujemy/Squash.java
+++ src/main/java/Testujemy/Squash.java
@@ -5,28 +5,6 @@ public class Squash {
     String amen;
     String mamen;
     int min;
-    int bombom;
 
-    public Squash(String amen, String mamen, int min) {
-        this.amen = amen;
-        this.mamen = mamen;
-        this.min = min;
-    }
-
-    public String getAmen() {
-        return amen;
-    }
-
-    public String getMamen() {
-        return mamen;
-    }
-
-    public int getMin() {
-        return min;
-    }
-
-    public void setAmen(String amen) {
-        this.amen = amen;
-    }
 
 }
