74c773adb9dc67d7a55819a7150e66808b3fc539
pusio <kpstrowski@gmail.com>
2021-10-23 13:54:28 +0200

    Gettery i settery

--- src/main/java/Testujemy/Osoba.java
+++ src/main/java/Testujemy/Osoba.java
@@ -12,5 +12,27 @@ public class Osoba {
         this.dob = dob;
     }
 
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getDob() {
+        return dob;
+    }
+
+    public void setDob(int dob) {
+        this.dob = dob;
+    }
 }
