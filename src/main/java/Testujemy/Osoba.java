3ad765644a6f0f0e2e5eb35e3bb95ddb7a4b9ce3
pusio <kpstrowski@gmail.com>
2021-10-23 13:53:09 +0200

    Dodany autor

---
+++ src/main/java/Testujemy/Osoba.java
@@ -0,0 +1,16 @@
+package Testujemy;
+
+public class Osoba {
+
+    String name;
+    String surname;
+    int dob;
+
+    public Osoba(String name, String surname, int dob) {
+        this.name = name;
+        this.surname = surname;
+        this.dob = dob;
+    }
+
+
+}
