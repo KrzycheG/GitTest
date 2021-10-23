b2f6c5d6cca3739e4f3345811571d5ddaf6db68c
pusio <kpstrowski@gmail.com>
2021-10-23 14:11:30 +0200

    Getter setter
    Getter setter
    
    Getter
    
    Konstruktor
    
    Stworzenie Squash
    
    toString

---
+++ src/main/java/Testujemy/Squash.java
@@ -0,0 +1,31 @@
+package Testujemy;
+
+public class Squash {
+
+    String amen;
+    String mamen;
+    int min;
+
+    public Squash(String amen, String mamen, int min) {
+        this.amen = amen;
+        this.mamen = mamen;
+        this.min = min;
+    }
+
+    public String getAmen() {
+        return amen;
+    }
+
+    public String getMamen() {
+        return mamen;
+    }
+
+    public int getMin() {
+        return min;
+    }
+
+    public void setAmen(String amen) {
+        this.amen = amen;
+    }
+
+}
